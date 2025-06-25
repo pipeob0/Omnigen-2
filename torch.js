ff9df5ce920f7f0e389e8fc973ce8cd03224fde5
pipeob0 <36382709+pipeob0@users.noreply.github.com>
2025-06-25 10:34:29 +0200

    Update torch.js

--- torch.js
+++ torch.js
@@ -24,7 +24,7 @@ module.exports = {
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
           "uv pip install torch torchvision torchaudio {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu124",
-          "{{args && args.triton ? 'uv pip install -U triton-windows --force-reinstall' : ''}}",
+          "{{args && args.triton ? 'uv pip install -U triton-windows==3.2.0.post19 --force-reinstall' : ''}}",
           "{{args && args.sageattention ? 'uv pip install https://github.com/deepbeepmeep/SageAttention/raw/refs/heads/main/releases/sageattention-2.1.0-cp310-cp310-win_amd64.whl --force-reinstall' : ''}}"
         ]
       },
